65c5003c7e8907a0029407ad88466e5713beed8c
Alan-Underground <alan_underground07@hotmail.com>
2019-09-03 20:41:53 -0300

    Adicionado String str, imprimindo valor da String str

--- aula_01/src/br/edu/udc/PrimeiraClasse.java
+++ aula_01/src/br/edu/udc/PrimeiraClasse.java
@@ -4,8 +4,9 @@ public class PrimeiraClasse {
 
 	public static void main(String[] args) {
 		int valor = 100;
+		String str = "Uma frase!";
 		System.out.println("Primeiro programa java!!!");	//forma de pular linha em java 
-			
+		System.out.println(str);	
 		System.out.printf("Mostrando um número %d\n", valor); //forma de pular linha em C
 	}
 
